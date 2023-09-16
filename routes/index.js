9f2cce35f4d354ec7e75fc22065e4117e51242e8
mr.beale <j.b.beale2015@gmail.com>
2023-09-15 22:57:21 -0400

    Finished project for week 1

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 const contactController = require('../controllers/contacts');
 
+router.get('/', (req, res) => { res.send('Week 1 - Contacts Part 1') });
 router.get('/contacts', contactController.getAll);
 router.get('/contacts/:id', contactController.getSingle);
 
