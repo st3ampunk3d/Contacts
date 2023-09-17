11d5a726ab04a4945c02959c4fab441cf6894be5
mr.beale <j.b.beale2015@gmail.com>
2023-09-16 22:15:25 -0400

    Added name to index. Removed unneccessary files.

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const contactController = require('../controllers/contacts');
 
-router.get('/', (req, res) => { res.send('Week 1 - Contacts Part 1') });
+router.get('/', (req, res) => { res.send('Joshua Beale | CSE 341 - Week 1 | Contacts Part 1') });
 router.get('/contacts', contactController.getAll);
 router.get('/contacts/:id', contactController.getSingle);
 
